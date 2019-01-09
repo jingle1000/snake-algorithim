1213317ba9f11bb77a5705a2edb4d0f454d3d394
Johnathan Ingle <jingle1000@gmail.com>
2019-01-09 12:49:56 -0500

    Push code before major change

--- snake.py
+++ snake.py
@@ -1,7 +1,31 @@
-
+import itertools
 snake_numbers = [[8,3,4,5],[1,4,2,3],[6,2,3,9],[4,4,2,6]]
 answers = [7,6,1,5]
+operators = ["x", '/', '+', '-']
+combinations = itertools.product(*snake_numbers)
 
+for combo in combinations:
+    for answer in answers:
+        a = combo[0]
+        b = combo[1]
+        c = combo[2]
+        d = combo[3]
+        if a*b*c*d is answer:
+            print(f'{a}{b}{c}{d}')
+        if a*b*c/d is answer:
+            print(f'{a}{b}{c}{d}')
+        if (a*b*c)+d is answer:
+            print(f'{a}{b}{c}{d}')
+        if (a*b*c)-d is answer:
+            print(f'{a}{b}{c}{d}')
+        if a*b/c*d is answer:
+            print(f'{a}{b}{c}{d}')
+        if a*b/c/d is answer:
+            print(f'{a}{b}{c}{d}')
+        if (a*b/c)+d is answer:
+            print(f'{a}{b}{c}{d}')
+        else:
+            pass #modify code from here
 
 
     
